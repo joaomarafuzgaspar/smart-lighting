3463173ad80663ad928f6501c76d78a81233fe27
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2023-04-03 15:26:58 +0100

    Moved `struct Node` into its own class

--- main/consensus.cpp
+++ main/consensus.cpp
@@ -1,27 +1,18 @@
+#include "consensus.h"
 #include <iostream>
-#include <vector>
 #include <limits>
 #include <cmath>
 
-// Assuming Node is a struct or class with appropriate properties
-struct Node
-{
-    double d_av, y, c, cost;
-    std::vector<double> k, o, l;
-    std::vector<double> d;
-    int n, m, index;
-};
-
-bool check_feasibility(const Node &node, const std::vector<double> &d)
+bool Node::check_feasibility(const std::vector<double> &d) const
 {
     const double tol = 0.001; // tolerance for rounding errors
 
-    if (d[node.index] < -tol)
+    if (d[this->index] < -tol)
     {
         return false;
     }
 
-    if (d[node.index] > 100 + tol)
+    if (d[this->index] > 100 + tol)
     {
         return false;
     }
@@ -29,10 +20,10 @@ bool check_feasibility(const Node &node, const std::vector<double> &d)
     double d_dot_k = 0;
     for (size_t i = 0; i < d.size(); ++i)
     {
-        d_dot_k += d[i] * node.k[i];
+        d_dot_k += d[i] * this->k[i];
     }
 
-    if (d_dot_k < node.L[node.index] - node.o[node.index] - tol)
+    if (d_dot_k < this->l[this->index] - this->o[this->index] - tol)
     {
         return false;
     }
@@ -40,103 +31,104 @@ bool check_feasibility(const Node &node, const std::vector<double> &d)
     return true;
 }
 
-double evaluate_cost(const Node& node, const std::vector<double>& d, double rho) {
-    double cost = 0.0;
+double Node::evaluate_cost(const std::vector<double>& d, double rho) const
+{
+  double cost = 0.0;
 
-    for (size_t i = 0; i < d.size(); ++i) {
-        cost += node.c * d[i] + node.y * (d[i] - node.d_av);
-    }
+  for (size_t i = 0; i < d.size(); ++i) {
+      cost += this->c * d[i] + this->y * (d[i] - this->d_av);
+  }
 
-    double norm_squared = 0.0;
-    for (size_t i = 0; i < d.size(); ++i) {
-        norm_squared += std::pow(d[i] - node.d_av, 2);
-    }
+  double norm_squared = 0.0;
+  for (size_t i = 0; i < d.size(); ++i) {
+      norm_squared += std::pow(d[i] - this->d_av, 2);
+  }
 
-    cost += rho / 2 * norm_squared;
+  cost += rho / 2 * norm_squared;
 
-    return cost;
+  return cost;
 }
 
-Node consensus_iterate(const Node &node, double rho, std::vector<double> &d, double &cost)
+Node& Node::consensus_iterate(double rho, std::vector<double> &d, double &cost)
 {
-    std::vector<double> d_best(2, -1);
-    double cost_best = 1000000; // large number
-    double z = rho * node.d_av - node.y - node.c;
+  std::vector<double> d_best(2, -1);
+  double cost_best = 1000000; // large number
+  double z = rho * this->d_av - this->y - this->c;
 
-    // unconstrained minimum
-    double d_u = (1 / rho) * z;
-    if (check_feasibility(node, {d_u}))
-    {
-        double cost_unconstrained = evaluate_cost(node, {d_u}, rho);
-        if (cost_unconstrained < cost_best)
-        {
-            d_best = {d_u};
-            cost_best = cost_unconstrained;
-        }
-    }
+  // unconstrained minimum
+  double d_u = (1 / rho) * z;
+  if (check_feasibility({d_u}))
+  {
+      double cost_unconstrained = evaluate_cost({d_u}, rho);
+      if (cost_unconstrained < cost_best)
+      {
+          d_best = {d_u};
+          cost_best = cost_unconstrained;
+      }
+  }
 
-    // compute minimum constrained to linear boundary
-    double d_bl = (1 / rho) * z - node.k[node.index] / node.n * (node.o[node.index] - node.l[node.index] + (1 / rho) * z * node.k[node.index]);
-    if (check_feasibility(node, {d_bl}))
-    {
-        double cost_boundary_linear = evaluate_cost(node, {d_bl}, rho);
-        if (cost_boundary_linear < cost_best)
-        {
-            d_best = {d_bl};
-            cost_best = cost_boundary_linear;
-        }
-    }
+  // compute minimum constrained to linear boundary
+  double d_bl = (1 / rho) * z - this->k[this->index] / this->n * (this->o[this->index] - this->l[this->index] + (1 / rho) * z * this->k[this->index]);
+  if (check_feasibility({d_bl}))
+  {
+      double cost_boundary_linear = evaluate_cost({d_bl}, rho);
+      if (cost_boundary_linear < cost_best)
+      {
+          d_best = {d_bl};
+          cost_best = cost_boundary_linear;
+      }
+  }
 
-    // compute minimum constrained to 0 boundary
-    double d_b0 = (1 / rho) * z;
-    if (check_feasibility(node, {d_b0, 0.0}))
-    {
-        double cost_boundary_0 = evaluate_cost(node, {d_b0, 0.0}, rho);
-        if (cost_boundary_0 < cost_best)
-        {
-            d_best = {d_b0, 0.0};
-            cost_best = cost_boundary_0;
-        }
-    }
+  // compute minimum constrained to 0 boundary
+  double d_b0 = (1 / rho) * z;
+  if (check_feasibility({d_b0, 0.0}))
+  {
+      double cost_boundary_0 = evaluate_cost({d_b0, 0.0}, rho);
+      if (cost_boundary_0 < cost_best)
+      {
+          d_best = {d_b0, 0.0};
+          cost_best = cost_boundary_0;
+      }
+  }
 
-    // compute minimum constrained to 100 boundary
-    double d_b1 = (1 / rho) * z;
-    if (check_feasibility(node, {d_b1, 100.0}))
-    {
-        double cost_boundary_100 = evaluate_cost(node, {d_b1, 100.0}, rho);
-        if (cost_boundary_100 < cost_best)
-        {
-            d_best = {d_b1, 100.0};
-            cost_best = cost_boundary_100;
-        }
-    }
+  // compute minimum constrained to 100 boundary
+  double d_b1 = (1 / rho) * z;
+  if (check_feasibility({d_b1, 100.0}))
+  {
+      double cost_boundary_100 = evaluate_cost({d_b1, 100.0}, rho);
+      if (cost_boundary_100 < cost_best)
+      {
+          d_best = {d_b1, 100.0};
+          cost_best = cost_boundary_100;
+      }
+  }
 
-    // compute minimum constrained to linear and 0 boundary
-    double d_l0 = (1 / rho) * z - (1 / node.m) * node.k[node.index] * (node.o[node.index] - node.L[node.index]) + (1 / rho / node.m) * node.k[node.index] * (node.k[node.index] * z - z * node.k[node.index]);
-    if (check_feasibility(node, {d_l0, 0.0}))
-    {
-        double cost_linear_0 = evaluate_cost(node, {d_l0, 0.0}, rho);
-        if (cost_linear_0 < cost_best)
-        {
-            d_best = {d_l0, 0.0};
-            cost_best = cost_linear_0;
-        }
-    }
+  // compute minimum constrained to linear and 0 boundary
+  double d_l0 = (1 / rho) * z - (1 / this->m) * this->k[this->index] * (this->o[this->index] - this->l[this->index]) + (1 / rho / this->m) * this->k[this->index] * (this->k[this->index] * z - z * this->k[this->index]);
+  if (check_feasibility({d_l0, 0.0}))
+  {
+      double cost_linear_0 = evaluate_cost({d_l0, 0.0}, rho);
+      if (cost_linear_0 < cost_best)
+      {
+          d_best = {d_l0, 0.0};
+          cost_best = cost_linear_0;
+      }
+  }
 
-    // compute minimum constrained to linear and 100 boundary
-    double d_l1 = (1 / rho) * z - (1 / node.m) * node.k[node.index] * (node.o[node.index] - node.L[node.index] + 100 * node.k[node.index]) + (1 / rho / node.m) * node.k[node.index] * (node.k[node.index] * z - z * node.k[node.index]);
-    if (check_feasibility(node, {d_l1, 100.0}))
-    {
-        double cost_linear_100 = evaluate_cost(node, {d_l1, 100.0}, rho);
-        if (cost_linear_100 < cost_best)
-        {
-            d_best = {d_l1, 100.0};
-            cost_best = cost_linear_100;
-        }
-    }
+  // compute minimum constrained to linear and 100 boundary
+  double d_l1 = (1 / rho) * z - (1 / this->m) * this->k[this->index] * (this->o[this->index] - this->l[this->index] + 100 * this->k[this->index]) + (1 / rho / this->m) * this->k[this->index] * (this->k[this->index] * z - z * this->k[this->index]);
+  if (check_feasibility({d_l1, 100.0}))
+  {
+      double cost_linear_100 = evaluate_cost({d_l1, 100.0}, rho);
+      if (cost_linear_100 < cost_best)
+      {
+          d_best = {d_l1, 100.0};
+          cost_best = cost_linear_100;
+      }
+  }
 
-    node.d = d_best;
-    node.cost = cost_best;
+  this->d = d_best;
+  this->cost = cost_best;
 
-    return node
+  return *this;
 }
--- main/consensus.h
+++ main/consensus.h
@@ -1,8 +1,17 @@
 #ifndef CONSENSUS_H
 #define CONSENSUS_H
 
-bool check_feasibility(const Node &node, const std::vector<double> &d);
-double evaluate_cost(const Node& node, const std::vector<double>& d, double rho);
-Node consensus_iterate(const Node &node, double rho, std::vector<double> &d, double &cost);
+#include <vector>
+
+class Node {
+  double d_av, y, c, cost;
+  std::vector<double> k, o, l;
+  std::vector<double> d;
+  int n, m, index;
+
+  bool check_feasibility(const std::vector<double> &d) const;
+  double evaluate_cost(const std::vector<double>& d, double rho) const; 
+  Node& consensus_iterate(double rho, std::vector<double> &d, double &cost);
+};
 
 #endif
